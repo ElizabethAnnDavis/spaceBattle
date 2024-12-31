8227e07100a0a5ae51e7883b9db1b68f06626057
ElizabethAnnDavis <immakitty88@gmail.com>
2024-12-30 21:45:20 -0500

    play button added/removed onclick --> begins game

--- app2.js
+++ app2.js
@@ -1,57 +1,3 @@
-/*
- * Space Battle OOP Lab
- * 
- * Today, you will be using your knowledge of OOP, loops, and functions to build a rudimentary space battle game.
- * The game will be programmed for, and played using window.prompt to get input from the user and buttons in the browser, 
- * you can also use console.log and window.alert. This is your first mini-project. 
- * You should not style the page until you first get all the functionality down.
- * 
- * 
- * SPECIFICATIONS:
- * Build a game of battling alien spaceships using Javascript.
- * Earth has been attacked by a horde of aliens! 
- * You are the captain of the USS Assembly, on a mission to destroy every last alien ship.
- * Battle the aliens as you try to destroy them with your lasers.
- * There are six alien ships. 
- * The aliens' weakness is that they are too logical and attack one at a time: 
- * - they will wait to see the outcome of a battle before deploying another alien ship. 
- * Your strength is that you have the initiative and get to attack first. 
- * However, you do not have targeting lasers and can only attack the aliens in order. 
- * After you have destroyed a ship, you have the option to make a hasty retreat.
- * 
- * A game round would look like this:
- * - You attack the first alien ship
- * - If the ship survives, it attacks you
- * - If you survive, you attack the ship again
- * - If it survives, it attacks you again ... etc
- * - If you destroy the ship, you have the option to attack the next ship or to retreat
- * - If you retreat, the game is over, perhaps leaving the game open for further developments or options
- * - You win the game if you destroy all of the aliens
- * - You lose the game if you are destroyed
- * 
- * Ship Properties:
- * - hull is the same as hitpoints. If hull reaches 0or less, the ship is destroyed
- * - firepower is the amount of damage done to the hull of the target with a successful hit
- * - accuracy is the chance between 0 and 1 that the ship will hit its target
- * 
- * Your spaceship, the USS Assembly should have the following properties:
- * - hull - 20
- * - firepower - 5
- * - accuracy - .7
- * 
- * The alien ships should each have the following ranged properties determined randomly:
- * - hull - between 3and 6
- * - firepower - between 2and 4
- * - accuracy - between .6and .8
- * 
- * You could be battling six alien ships each with unique values.
- * 
- */
-
-
-
-
-
 const topOfBoard = document.getElementById('row1');
 const middleOfBoard = document.getElementById('row4');
 const bottomOfBoard = document.getElementById('row8');
@@ -184,10 +130,8 @@ class Invader extends SpaceShip{
 let isFirstGame = true;
 let keepPlayingGame = true;
 
-function startGame(e){
+function startGame(){//e){
     // reset game
-    resetGame();
-
     if(!isFirstGame){
         /* TEST CODE: */
         console.log("FIRST RUN: " + isFirstGame);
@@ -202,7 +146,7 @@ function startGame(e){
 
     playGame();
 
-}playBtn.addEventListener('click', startGame)
+}//playBtn.addEventListener('click', startGame)
 
 function playGame(){
     // create characters
@@ -265,7 +209,6 @@ function playGame(){
 function resetGame(){
     // clear old game
     // remove playBtn
-    middleOfBoard.removeChild(playBtn);
     // place inital characters
     // set playGame = true
 }
@@ -276,20 +219,19 @@ function gameOver(str){
     // display playBtn
 }
 
-/*
+
 do{
     for(let i = 0; i < 3; i++){
-        //* TEST CODE: 
+        /* TEST CODE: */
         //console.log("Game Loop: " + (i+1));
         //startGame();
         //if(i === 2){
           //  keepPlayingGame = false;
         //};
-        //* TEST CODE: 
+        /* TEST CODE: */
         //console.log("Play Again: " + keepPlayingGame);
     };
     startGame();
     keepPlayingGame = false;
 
 }while(keepPlayingGame === true);
-*/
